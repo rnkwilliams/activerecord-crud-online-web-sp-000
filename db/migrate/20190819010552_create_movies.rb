bbdc90e719e765485cd242367dfea349ac05f9ac
Ranika Williams <rnkprice07@gmail.com>
2019-08-19 13:24:56 +0000

    Automatically backed up by Learn

--- db/migrate/20190819010552_create_movies.rb
+++ db/migrate/20190819010552_create_movies.rb
@@ -3,6 +3,8 @@ class CreateMovies < ActiveRecord::Migration[5.2]
     create_table :movies do |t|
       t.string :title
       t.integer :release_date
-      
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
   end
 end
